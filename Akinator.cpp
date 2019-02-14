fdab1a0fdbb63bd1aa18379bb532f28f28199b5d
Grigoriy <44481229+Grigonoid@users.noreply.github.com>
2019-02-14 19:47:20 +0300

    Add files via upload

--- Akinator.cpp
+++ Akinator.cpp
@@ -1,17 +1,20 @@
 #include <stdio.h> 
-#include <string.h>
+#include <string>
 #include <stdlib.h>
+#include <iostream>
+
+const int ERROR = -2;
 
 struct Node 
 {
-	char* m_data;
+	std::string m_data;
 	Node* m_left;
 	Node* m_right;
 	Node* m_parent;
 
-	Node(char* data, Node* left = NULL, Node* right = NULL, Node* parent = NULL);
+	Node(std::string data, Node* left = NULL, Node* right = NULL, Node* parent = NULL);
 	~Node();
-	void NewBranch(char* name, char* trait);
+	void NewBranch(std::string name, std::string trait);
 };
 
 struct Tree
@@ -21,8 +24,8 @@ struct Tree
 	Tree();
 	Node* Guess();
 	void CheckAndAdd(Node* cur);
-	//void Write(char* filename);
-	//void Read(char* filename);
+	void Write(FILE* write_file);
+	Node* Read(FILE* read_file);
 };
 
 struct Game
@@ -31,40 +34,38 @@ struct Game
 	
 	void Choice();
 	void Play();
+	int Save(char* filename);
+	int Load(char* filename);
 };
 
-Node::Node(char* data, Node* left, Node* right, Node* parent):
+Node::Node(std::string data, Node* left, Node* right, Node* parent):
 	m_left(left),
 	m_right(right),
 	m_parent(parent)
 	{
-		m_data = new char[strlen(data) + 1];
-		strcpy(m_data, data);
+		m_data = data;
 	}
 	
 Node::~Node()
 {
-	delete[] m_data;
+	m_data = "This object has been destructed.";
 }
 	
-void Node::NewBranch(char* name, char* trait)
+void Node::NewBranch(std::string name, std::string trait)
 {
 	m_right = new Node(name);
 	this->m_right->m_parent = this;
 	m_left = new Node(m_data);
 	this->m_left->m_parent = this;
-	char question[269] = "Ваш персонаж ";
-	strcat(question, trait);
-	delete[] m_data;
-	m_data = new char[strlen(question) + 1];
-	strcpy(m_data, question);
+	m_data = "Ваш персонаж ";
+	m_data += trait;
 }
 
 Tree::Tree()
 {
-	char question[] = "Ваш персонаж реален";
-	char no[] = "Человек-паук";
-	char yes[] = "Андрей Егоров";
+	std::string question = "Ваш персонаж реален";
+	std::string no = "Человек-паук";
+	std::string yes = "Овчинкин";
 	m_root = new Node(question, new Node(no), new Node(yes));
 	m_root->m_left->m_parent = m_root;
 	m_root->m_right->m_parent = m_root;
@@ -73,42 +74,105 @@ Tree::Tree()
 Node* Tree::Guess()
 {
 	Node* cur = m_root;
-	char yes[] = "Да"; 
-	char no[] = "Нет";
-	char answer[1000] = "";
+	std::string yes = "Да"; 
+	std::string no = "Нет";
+	std::string answer;
 	for (;cur->m_right != NULL;)
 	{
-		printf("%s?[Да/Нет]\n", cur->m_data);
-		gets(answer);
-		if (strcmp(yes, answer) == 0) cur = cur->m_right;
-		else if (strcmp(no, answer) == 0) cur = cur->m_left;
+		printf("%s?[Да/Нет]\n", cur->m_data.c_str());
+		std::getline(std::cin, answer);
+		if (answer == yes) cur = cur->m_right;
+		else if (answer == no) cur = cur->m_left;
 		else printf("Ответ должен быть дан одним словом: Да или Нет.\n");
 	}
-	printf("Это %s.\n", cur->m_data);
+	printf("Это %s.\n", cur->m_data.c_str());
 	return cur;
 }
 
 void Tree::CheckAndAdd(Node* cur)
 {
 	printf("Верно?[Да/Нет]\n");
-	char yes[] = "Да"; 
-	char no[] = "Нет";
-	char answer[1000] = "";
-	gets(answer);
-	while (strcmp(no, answer) != 0 && strcmp(yes, answer) != 0) {
+	std::string yes = "Да"; 
+	std::string no = "Нет";
+	std::string answer;
+	std::getline(std::cin, answer);
+	while ((answer != no) && (answer != yes)) {
 		printf("Ответ должен быть дан одним словом: Да или Нет.\n");
-		gets(answer);
+		std::getline(std::cin, answer);
 	}
-	if (strcmp(no, answer) == 0) {
+	if (answer == no) {
 		printf("Какого персонажа вы загадали?\n");
-		char name[256] = "";
-		gets(name);
+		std::string name;
+		std::getline(std::cin, name);
 		printf("Напишите признак, который отличает вашего персонажа от угаданного.\n");
-		char trait[256] = "";
-		gets(trait);
+		std::string trait;
+		std::getline(std::cin, trait);
 		cur->NewBranch(name, trait);
 	}
 }
+
+void Tree::Write(FILE* write_file)
+{
+	char c = '[';
+	fwrite(&c, sizeof(char), 1, write_file);
+	size_t str_size = m_root->m_data.size();
+	fwrite(&str_size, sizeof(size_t), 1, write_file);
+	fwrite(m_root->m_data.data(), sizeof(char), str_size, write_file);
+	c = '_';
+	char n = 'N';
+	if (m_root->m_left != NULL) {
+		fwrite(&c, sizeof(char), 1, write_file);
+		m_root = m_root->m_left;
+		this->Write(write_file);
+		m_root = m_root->m_parent;
+	}
+	else fwrite(&n, sizeof(char), 1, write_file);
+	if (m_root->m_right != NULL) {
+		fwrite(&c, sizeof(char), 1, write_file);
+		m_root = m_root->m_right;
+		this->Write(write_file);
+		m_root = m_root->m_parent;
+	}
+	else fwrite(&n, sizeof(char), 1, write_file);
+	c = ']';
+	fwrite(&c, sizeof(char), 1, write_file);
+}
+
+Node* Tree::Read(FILE* read_file)
+{
+	char c = 0;
+	fread(&c, sizeof(char), 1, read_file);
+	size_t str_size = 0;
+	fread(&str_size, sizeof(size_t), 1, read_file);
+	std::string str(str_size, '\0');
+	fread(&str[0], sizeof(char), str_size, read_file);
+	m_root = new Node(str);
+	fread(&c, sizeof(char), 1, read_file);
+	if (c == '_') {
+		Node temp_node_l("Это временный узел");
+		m_root->m_left = &temp_node_l;
+		Node* temp_root = m_root;
+		m_root = m_root->m_left;
+		Node* new_left = this->Read(read_file);
+		m_root = temp_root;
+		m_root->m_left = new_left;
+		m_root->m_left->m_parent = m_root;
+	}
+	fread(&c, sizeof(char), 1, read_file);
+	if (c == '_') {
+		Node temp_node_r("Это временный узел");
+		m_root->m_right = &temp_node_r;
+		m_root->m_right = &temp_node_r;
+		Node* temp_root = m_root;
+		m_root = m_root->m_right;
+		Node* new_right = this->Read(read_file);
+		m_root = temp_root;
+		m_root->m_right = new_right;
+		m_root->m_right->m_parent = m_root;
+	}
+	fread(&c, sizeof(char), 1, read_file);
+	return m_root;
+}	
 
 char GetDigit_1to5()
 {
@@ -131,8 +195,15 @@ void Game::Choice()
 		case '1': 
 		Play();
 		break;
-		case '2': 
+		case '2':
+		Save("savefile.txt");
+		printf("\nВаш прогресс был успешно созранён!\n");
+		Choice();
+		break;
 		case '3':
+		Load("savefile.txt");
+		Choice();
+		break;
 		case '4':
 		printf("\nДанная опция находится в разработке. Извините за предоставленные неудобства.\n");
 		Choice();
@@ -149,9 +220,30 @@ void Game::Play()
 	Choice();
 }
 
+int Game::Save(char* filename)
+{
+	FILE* write_file = fopen(filename, "wb");
+	if (write_file == NULL) 
+		return ERROR;
+	m_tree.Write(write_file);
+	fclose(write_file);
+	return 0;
+}
+
+int Game::Load(char* filename)
+{
+	FILE* read_file = fopen(filename, "rb");
+	if (read_file == NULL) {
+		printf("Не открывается файл\n");
+		return ERROR;
+	}
+	m_tree.Read(read_file);
+	fclose(read_file);
+	return 0;
+}
+
 int main()
 {
 	Game game;
 	game.Choice();
 }
-	
